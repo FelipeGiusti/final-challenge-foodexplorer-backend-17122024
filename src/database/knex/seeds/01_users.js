41cb675adc602f360b1908e4d9dfe5f9842f4dfa
Felipe Giusti <felipeegiusti@gmail.com>
2025-01-13 19:28:56 -0300

    ajuste

--- src/database/knex/seeds/01_users.js
+++ src/database/knex/seeds/01_users.js
@@ -7,16 +7,10 @@ exports.seed = async function (knex) {
   await knex('users').del();
   await knex('users').insert([
     {
-      name: 'John Doe',
-      email: 'user@email.com',
-      password: '$2a$10$q2u7xgVso1sHzfe4mBdKeu495O17Z307AXgaA9Ou.fBPeBZhGQEBO',
+      name: 'Felipe Giusti',
+      email: 'felipe@email.com',
+      password: '123456',
       admin: false,
-    },
-    {
-      name: 'Jane Doe',
-      email: 'admin@email.com',
-      password: '$2a$10$q2u7xgVso1sHzfe4mBdKeu495O17Z307AXgaA9Ou.fBPeBZhGQEBO',
-      admin: true,
     },
   ]);
 };
