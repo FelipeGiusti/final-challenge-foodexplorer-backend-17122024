4786c7f071d60c9c491d51ea08fa7f0127be3961
felipe giusti <felipeegiusti@gmail.com>
2025-01-14 09:35:07 -0300

    Erro deploy

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use(routes);
 app.use(appErrorMiddleware);
 
+app.get("/", (req, res) => {
+  return res.json({"message": "OK"});
+})
+
 app.listen(PORT, function () {
   console.log(`[Server] listening on port ${PORT}`);
 });
